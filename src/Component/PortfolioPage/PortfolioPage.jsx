23ac1e5e3d85cde9283b4c61841e5a33de132852
Samuel <ampahsamuel99@gmail.com>
2025-03-08 12:06:21 +0000

    Fix the image url in portfolio

--- src/Component/PortfolioPage/PortfolioPage.jsx
+++ src/Component/PortfolioPage/PortfolioPage.jsx
@@ -1,9 +1,4 @@
 import React, { useState } from "react";
-import ecommerce from "/ecommerce-website.png";
-import gamehub from "/game-hub.png";
-import weather from "/weather.jpg";
-import e_university from "/e_university.png"
-import burgerhaven from "/burgerhaven.png"
 
 
 const PortfolioPage = () => {
@@ -13,7 +8,7 @@ const PortfolioPage = () => {
       title: "E-University Website",
       description:
         "The E-University platform is designed to facilitate seamless online learning. Built with React and Tailwind CSS, it offers a user-friendly interface for students to access courses, view schedules, and interact with instructors.",
-      imageUrl: e_university,
+      imageUrl: "/e_university.png",
       githubUrl: "https://github.com/Ampah01/E_University",
       more_details:
         "This project integrates modern design principles to create an intuitive platform for both students and faculty. Users can browse courses and manage their schedules. React ensures a smooth, responsive interface, while Tailwind CSS enhances the site's scalability and performance.",
@@ -28,7 +23,7 @@ const PortfolioPage = () => {
       demoUrl: "https://burger-haven-theta.vercel.app/",
       more_details:
         "Experience BurgerHaven, a dynamic burger website built with React and Bootstrap for a seamless and responsive experience. Browse our delicious menu in a PDF format and easily place orders through the dedicated order page. With smooth navigation powered by React, you can explore our handcrafted burgers, customize your meal, and complete your order effortlessly.",
-      imageUrl: burgerhaven,
+        imageUrl: "/burgerhaven.png",
     },
     {
       id: 3,
@@ -39,14 +34,14 @@ const PortfolioPage = () => {
       demoUrl: "https://ecommerce-website-eight-zeta.vercel.app/",
       more_details:
         "Explore our sleek sneaker shop, crafted with React and Tailwind CSS for a seamless user experience. Browse a curated collection of stylish footwear, from classics to limited editions, all designed to elevate your style effortlessly. Shop with confidence and ease, navigating through intuitive React routes for quick access to product details and checkout.",
-      imageUrl: ecommerce,
+      imageUrl: "./ecommerce-website.png",
     },
     {
       id: 4,
       title: "Game hub",
       description:
         "The dynamic game discovery platform utilizes React, Tailwind CSS, and Axios to offer a responsive grid layout, real-time search functionality, and expandable game cards for enhanced user engagement.",
-      imageUrl: gamehub,
+      imageUrl: "/game-hub.png",
       githubUrl: "https://github.com/Ampah01/Game-hub",
       more_details:
         "This project combines React hooks and modern UI/UX principles to deliver a streamlined game browsing experience. It features a responsive design that adjusts seamlessly across devices, with interactive elements that improve user engagement. The search functionality is powered by React hooks for real-time filtering, while expandable game cards provide detailed information in a focused view. Tailwind CSS ensures a modern and scalable interface, enhancing overall usability and visual appeal.",
@@ -57,7 +52,7 @@ const PortfolioPage = () => {
       title: "Weather App",
       description:
         "This project is a Weather and To-Do application that integrates real-time weather data and task management into a single interface using React and Tailwind CSS. It features current weather information, a 7-day forecast from the OpenWeatherMap API, and a to-do list with functionalities to add, delete, and rearrange tasks.",
-      imageUrl: weather,
+      imageUrl: "/weather.jpg",
       githubUrl: "https://github.com/Ampah01/Weather-App",
       more_details:
         "It offers a seamless user experience by displaying weather data and task management tools side by side. With a visually appealing interface, it allows users to stay informed about the weather while keeping track of their daily tasks. The application also includes features such as search functionality for weather updates based on location and animated transitions for a more engaging user interaction.",
